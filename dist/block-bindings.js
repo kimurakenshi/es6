efa429d80c2a7838b0a098417c1cabc6c9b9a0d8
sgiraud <sebastian.giraudo@globant.com>
2016-01-27 14:03:44 -0600

    let declaration

---
+++ dist/block-bindings.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// Let declarations
+function getValue(condition) {
+
+  if (condition) {
+    var value = "blue";
+
+    // other code
+
+    return value;
+  } else {
+
+    // value doesn't exist here
+
+    return null;
+  }
+
+  // value doesn't exist here
+}
